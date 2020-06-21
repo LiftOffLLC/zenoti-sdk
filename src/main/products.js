3f5d9218bbb971bf3fe92e26df57b329d11611a4
Saikat Dutta <saikat@liftoffllc.com>
2020-06-21 19:27:45 +0530

    get products api updated

--- src/main/products.js
+++ src/main/products.js
@@ -3,7 +3,7 @@ const Rest = require("./helper/rest");
 class Products extends Rest {
   async fetch({ centerId }) {
     return await this.get(
-      `/v1/centers/${centerId}/products?expand=preferences`
+      `/v1/centers/${centerId}/products?expand=preferences&expand=catalog_info`
     );
   }
 }
