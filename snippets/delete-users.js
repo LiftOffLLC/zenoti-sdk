267d99900699fae580df0c9533251f47055ea8c0
sspathare97 <sspathare97@gmail.com>
2020-11-06 12:40:44 +0530

    updated delete snippet

--- snippets/delete-users.js
+++ snippets/delete-users.js
@@ -1,28 +1,35 @@
 require("dotenv").config();
-const ZenotiClient = require("../main");
+const ZenotiClient = require("../src/main");
 
 const options = {
   handler: async (request, _h) => {
     const client = new ZenotiClient();
     const centerId = "aad7b465-3d5c-4421-be94-7ba021ee917d";
     // eslint-disable-next-line no-await-in-loop
-    const response = await client.guests.get(
-      `/v1/guests?center_id=${centerId}&size=100&page=1`
-    );
+    let x = [];
+    let page = 1;
+    let response;
+    do {
+      response = await client.guests.get(
+        `/v1/guests?center_id=${centerId}&size=100&page=${page}`
+      );
+      x = x.concat(response.guests);
+      page++;
+    } while (response.guests.length);
     // console.dir(guests, { depth: null });
-    const x = response.guests;
-    const m = ["EMP01", "EMP02", "MGR01", "THP01", "THP02"];
+    console.log(`${x.length} total guests`);
+      const m = ["EMP01", "EMP02", "MGR01", "THP01", "THP02"];
     const yy = x.filter((e) => !m.includes(e.code));
     // const y = yy.filter(e=>(e.code))
     const z = yy.filter((m) => !/.*\d{13}/.test(m.personal_info.email));
     if (!z.length) {
       console.log("No guests without disabled emails!");
     } else {
-      console.log("Guests without disabled emails:");
-      console.log(z.length);
+      console.log(`${z.length} guests without disabled emails:`);
       for (const user of z) {
-        console.log(user);
-        if (user.code === "OH-J-2") {
+        const u = (({ id, code, personal_info: {first_name, last_name, email} }) => ({ id, code, first_name, last_name, email }))(user);
+        console.log(u);
+        if (user.code === "OH-S-2" || user.code === "OH-S-4" || user.code === "OH-S-6") {
           const emailWithTimestamp = user.personal_info.email + Date.now();
           const ohDisabledId = user.code
             ? `D-${user.code}-${Date.now()}`
