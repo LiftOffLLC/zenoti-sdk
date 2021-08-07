26a51d83181c742670f74553d7b374450680dd21
John Francis <johnfrancis95815@gmail.com>
2021-08-07 23:16:59 +0530

    change membership to force auth

--- src/main/memberships.js
+++ src/main/memberships.js
@@ -1,19 +1,16 @@
 const Rest = require("./helper/rest");
 
 class Memberships extends Rest {
-  async create({
-    centerId,
-    userId,
-    membershipId,
-  }) {
+  async create({ centerId, userId, membershipId }) {
     return await this.post(
-      '/v1/invoices/memberships',
+      "/v1/invoices/memberships",
       {},
       {
         center_id: centerId,
         user_id: userId,
         membership_ids: membershipId,
       },
+      true
     );
   }
 }
