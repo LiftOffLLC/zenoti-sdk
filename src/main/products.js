845f4d14cdcee2e99a3c09b9fc857c403c79b138
pcpranav <pcpranavchandra@gmail.com>
2022-01-18 11:13:31 +0530

    products update

--- src/main/products.js
+++ src/main/products.js
@@ -16,9 +16,7 @@ class Products extends Rest {
   }
 
   async fetch({ centerId }) {
-    return await this.get(
-      `/v1/centers/${centerId}/products?expand=preferences&expand=tags&expand=catalog_info&expand=barcodes&show_in_catalog=true&expand=centers`
-    );
+    return await this.get(`/v1/centers/${centerId}/products`);
   }
 }
 
